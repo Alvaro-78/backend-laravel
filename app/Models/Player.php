23d995091f96f48390957168b5b37c77636ca2ee
Álvaro Aguerré Navarrete <alvaro78an@gmail.com>
2021-04-14 19:25:37 +0200

    Add game,message,player and party models

---
+++ app/Models/Player.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Player extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['nickName', 'password', 'email'];
+
+    public function playerParty() {
+        return $this -> belongsTo('App/Models/Party/', 'idplayer');
+    }
+}
