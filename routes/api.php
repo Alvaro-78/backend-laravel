70fc8c240e568d775333f21eaaeeac8ae558b321
@carlosval <75422798+Carlos-Val@users.noreply.github.com>
2021-04-16 19:50:07 +0200

    add route endpoint create party

--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,12 @@ use App\Http\Controller\PlayerController;
 
 Route::get('/player/{nickName}', [PlayerController::class, 'playerName']);
 Route::post('/player', [PlayerController::class, 'registerPlayer']);
+Route::post('/player/login', [PlayerController::class, 'loginPlayer']);
+Route::post('/player/logout', [PlayerController::class, 'logOutPlayer'])->middleware('token');
 Route::put('/player', [PlayerController::class, 'modifyPlayer']);
+Route::post('/player/party', [PlayerController::class, 'createParty']);
+
+
+
 
 
