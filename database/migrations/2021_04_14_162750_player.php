59ae98bf7c08e749066ed7e3d88982fc46b7369f
@carlosval <75422798+Carlos-Val@users.noreply.github.com>
2021-04-16 19:46:28 +0200

    created migrations from player, game, message and party

--- database/migrations/2021_04_14_162750_player.php
+++ database/migrations/2021_04_14_162750_player.php
@@ -18,6 +18,7 @@ class Player extends Migration
             $table -> string('nickName') -> unique();
             $table -> string('password');
             $table -> string('email') -> unique();
+            $table->string('token')->nullable();
             $table -> timestamps();
         });
     }
---
+++ database/migrations/2021_04_16_113601_create_game.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGame extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('game', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('gameName');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('game');
+    }
+}
