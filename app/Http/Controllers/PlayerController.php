37711e00c7217b78b37d841345c076cf780ef7bf
@carlosval <75422798+Carlos-Val@users.noreply.github.com>
2021-04-16 19:49:17 +0200

    add created party in playerController

--- app/Http/Controllers/PlayerController.php
+++ app/Http/Controllers/PlayerController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Player;
+use App\Models\Party;
 use Illuminate\Database\QueryException;
 
 use Illuminate\Support\Facades\Hash;
@@ -12,6 +13,78 @@ use Illuminate\Support\Facades\Hash;
 class PlayerController extends Controller
 {
 
+    
+    //Login
+
+    public function loginPlayer(Request $request){
+
+        $nickName = $request->input('nickName');
+        $password = $request->input('password');
+
+        try{
+            //Miramos que el nickName existe en la tabla player
+
+            $validatePlayer = Player::select('password')
+            ->where('nickName', 'LIKE', $nickName)
+            ->first();
+
+            if(!$validatePlayer){
+                return response()->json([
+                    //nickName incorrecto
+                    'error'=> "Nickname o password incorrecto"
+                ]);
+            }
+
+            $hashed = $validatePlayer->password;
+
+            //Comprobar si password recibido corresponde con el del nickName del player
+
+            if(Hash::check($password, $hashed)){
+
+                //Genera token
+
+                $length = 35;
+                $token = bin2hex(random_bytes($length));
+
+                //Guardamos el token en su campo de la DB
+
+                Player::where('nickName', $nickName)
+                ->update(['token' => $token]);
+
+                //Devolvemos al front la info ya actualizada
+                return Player::where('nickName', 'LIKE', $nickName)
+                ->get();
+
+            }else{
+                return response()->json([
+                    //password incorrecto
+                    'error' => "Nickname o password incorrecto"
+                ]);
+            }
+        }catch(QueryException $error){
+            
+            return response()->$error;
+        }
+    }
+
+    //LogOut
+
+    public function logOutPlayer(Request $request){
+
+        $nickName = $request->input('nickName');
+
+        try{
+
+            return Player::where('nickName', '=', $nickName)
+            ->update(['token' => '']);
+
+        }catch(QueryException $error){
+            return $error;
+        }
+    }
+    
+    
+    
     // Buscamos el nombre del jugador
 
     public function playerName($nickName) {
@@ -73,4 +146,27 @@ class PlayerController extends Controller
             return $error;
         }
     }
+
+    //Crear party
+    public function createParty(Request $request){
+
+        $partyName = $request->input('partyName');
+        $idplayer = $request->input('idplayer');
+        $idgame = $request->input('idgame');
+        $idmessage = $request->input('idmessage');
+
+        try{
+
+            return Party::create([
+                'partyName' => $partyName,
+                'idplayer' => $idplayer,
+                'idgame' => $idgame,
+                'idmessage' => $idmessage
+            ]);
+        }catch(QueryException $error) {
+            return $error;
+        }
+    }
+
+
 }
