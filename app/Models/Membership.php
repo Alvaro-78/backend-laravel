86a8b27e2cd5f1c087ba80dd39305b0be2f2dd02
@carlosval <75422798+Carlos-Val@users.noreply.github.com>
2021-04-17 18:32:01 +0200

    created membership

---
+++ app/Models/Membership.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Membership extends Model
+{
+    use HasFactory;
+
+    public function membershipParty(){
+
+        return $this->belongsTo('App\Models\Party', 'idparty', 'id');
+
+    }
+
+    public function membershipPlayer(){
+
+        return $this->belongsTo('App\Models\Player', 'idplayer', 'id');
+        
+    }
+}
